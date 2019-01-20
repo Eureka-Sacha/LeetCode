c8bcbeaa36b6226e082d8344d6812928c6f0438f
奎 <Eureka_sacha@outlook.com>
2019-01-20 22:48:24 +0800

    update

--- src/com/eureka/ContainsDuplicate.java
+++ src/com/eureka/ContainsDuplicate.java
@@ -9,7 +9,8 @@ import java.util.Set;
  *
  * @date: @2019/1/20.
  * @mail:
- * @description: 给定一个整数数组，判断是否存在重复元素。
+ * @description: 217. 存在重复元素
+ * 给定一个整数数组，判断是否存在重复元素。
  * <p>
  * 如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
  * <p>
@@ -53,17 +54,17 @@ public class ContainsDuplicate {
         return false;
     }
 
-    public static void main(String[] args) {
-        int[] nums = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-        long start = System.currentTimeMillis();
-        System.out.println(containsDuplicateMyself1(nums));
-        System.out.println(Arrays.toString(nums));
-        System.out.println(System.currentTimeMillis() - start + "ms");
-        start = System.currentTimeMillis();
-        System.out.println(containsDuplicateMyself2(nums));
-        System.out.println(Arrays.toString(nums));
-        System.out.println(System.currentTimeMillis() - start + "ms");
+        public static void main(String[] args) {
+            int[] nums = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+            long start = System.currentTimeMillis();
+            System.out.println(containsDuplicateMyself1(nums));
+            System.out.println(Arrays.toString(nums));
+            System.out.println(System.currentTimeMillis() - start + "ms");
+            start = System.currentTimeMillis();
+            System.out.println(containsDuplicateMyself2(nums));
+            System.out.println(Arrays.toString(nums));
+            System.out.println(System.currentTimeMillis() - start + "ms");
 
-    }
+        }
 
 }
---
+++ src/com/eureka/SingleNumber.java
@@ -0,0 +1,48 @@
+package com.eureka;
+
+import java.util.Arrays;
+
+/**
+ * Created by @奎
+ *
+ * @date: @2019/1/20.
+ * @mail:
+ * @description: 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * <p>
+ * 说明：
+ * <p>
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [2,2,1]
+ * 输出: 1
+ * 示例 2:
+ * <p>
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ */
+public class SingleNumber {
+    public static int singleNumberMyself(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i += 2) {
+            if (i + 1 >= nums.length || nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0};
+        long start = System.currentTimeMillis();
+        System.out.println(singleNumberMyself(nums));
+        System.out.println(Arrays.toString(nums));
+        System.out.println(System.currentTimeMillis() - start + "ms");
+    }
+
+
+}
